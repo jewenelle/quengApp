034be3c15efbc539610bbd4f2989967c566960ab
jewenelle <71766699+jewenelle@users.noreply.github.com>
2023-02-14 00:36:22 +0800

    Update app.js
    first update

--- app.js
+++ app.js
@@ -16,12 +16,14 @@ let num = 0;
 input.addEventListener("keydown", (e) => {
   if (e.keyCode === 97 || e.keyCode === 98) {
     input.classList.add("hide");
-    head.classList.add("wobble");
+
     const headAnumation = () => {
-      head.classList.remove("wobble");
+      head.classList.remove("zoomIn");
+      tell2head.classList.remove("zoomIn");
     };
-    setTimeout(headAnumation, 1500);
-    heading.innerText = 1 + num++;
+    setTimeout(headAnumation, 1000);
+    //heading.innerText = 1 + num++;
+    //pN.innerText = 1 + num++;
 
     //let tellerKey = e.keyCode === 97 || e.keyCode === 98;
     input.value = "";
@@ -33,7 +35,7 @@ input.addEventListener("keydown", (e) => {
     function getVoices() {
       let voices = speechSynthesis.getVoices();
       if (!voices.length) {
-        let utterance = new SpeechSynthesisUtterance("");
+        let utterance = new SpeechSynthesisUtterance("now serving");
         speechSynthesis.speak(utterance);
 
         voices = speechSynthesis.getVoices();
@@ -44,20 +46,24 @@ input.addEventListener("keydown", (e) => {
 
     //let textToSpeak = "number" + num + "on teller one";
     if (e.keyCode === 97) {
-      head.innerText = `TELLER 2`;
-      let textToSpeak = "number" + num + "to payment two";
+      head.classList.add("zoomIn");
+      heading.innerText = 1 + num++;
+      head.innerText = `Window 1`;
+      let textToSpeak = "number" + num + "to window 1";
       let speakData = new SpeechSynthesisUtterance();
       speakData.volume = 1; // From 0 to 1
       speakData.rate = 1; // From 0.1 to 10
-      speakData.pitch = 3; // From 0 to 2
+      speakData.pitch = 2; // From 0 to 2
       speakData.text = textToSpeak;
       speakData.lang = "en";
       speakData.voice = getVoices()[0];
 
       speechSynthesis.speak(speakData);
     } else if (e.keyCode === 98) {
-      head.innerText = `TELLER 3`;
-      let textToSpeak = "number" + num + "to payment three";
+      tell2head.classList.add("zoomIn");
+      pN.innerText = 1 + num++;
+      tell2head.innerText = `WINDOW 2`;
+      let textToSpeak = "number" + num + "to WINDOW 2";
       let speakData = new SpeechSynthesisUtterance();
       speakData.volume = 1; // From 0 to 1
       speakData.rate = 1; // From 0.1 to 10
@@ -68,42 +74,6 @@ input.addEventListener("keydown", (e) => {
 
       speechSynthesis.speak(speakData);
     }
-  } else if (e.keyCode === 105) {
-    tell2head.classList.add("swing");
-    const headAnimation = () => {
-      tell2head.classList.remove("swing");
-    };
-    setTimeout(headAnimation, 1500);
-    pN.innerText = 1 + Value++;
-    //inputKey.value = "";
-    console.clear();
-
-    let utterance = new SpeechSynthesisUtterance("");
-    speechSynthesis.speak(utterance);
-
-    function getVoices() {
-      let voices = speechSynthesis.getVoices();
-      if (!voices.length) {
-        let utterance = new SpeechSynthesisUtterance("");
-        speechSynthesis.speak(utterance);
-
-        voices = speechSynthesis.getVoices();
-        //head.classList.add("bounce");
-      }
-      return voices;
-    }
-
-    //let textToSpeak = "number" + num + "on teller one";
-    let textToSpeak = "number" + Value + "to priority lane";
-    let speakData = new SpeechSynthesisUtterance();
-    speakData.volume = 1; // From 0 to 1
-    speakData.rate = 1; // From 0.1 to 10
-    speakData.pitch = 3; // From 0 to 2
-    speakData.text = textToSpeak;
-    speakData.lang = "en";
-    speakData.voice = getVoices()[0];
-
-    speechSynthesis.speak(speakData);
   } else {
     console.error;
   }
